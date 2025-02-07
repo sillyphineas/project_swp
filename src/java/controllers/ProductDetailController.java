4a9a6f23843717eaa53ebc7e3a8bb8140da9e144
Tran Duc Hai <haiductran712@gmail.com>
2025-02-08 02:21:14 +0700

    Revert "Dat dlq branch"

--- src/java/controllers/ProductDetailController.java
+++ src/java/controllers/ProductDetailController.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
+
 package controllers;
 
 import entities.Brand;
@@ -21,39 +22,36 @@ import models.DAOProduct;
  *
  * @author HP
  */
-@WebServlet(name = "ProductDetailController", urlPatterns = {"/ProductDetailController"})
+@WebServlet(name="ProductDetailController", urlPatterns={"/ProductDetailController"})
 public class ProductDetailController extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ProductDetailController</title>");
+            out.println("<title>Servlet ProductDetailController</title>");  
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet ProductDetailController at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet ProductDetailController at " + request.getContextPath () + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    }
+    } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
+    /** 
      * Handles the HTTP <code>GET</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -61,33 +59,23 @@ public class ProductDetailController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         DAOProduct dao = new DAOProduct();
         DAOBrand daoBrand = new DAOBrand();
-        Vector productList = new Vector();
-//        String searchQuery = request.getParameter("search");
-//        if (searchQuery != null && !searchQuery.trim().isEmpty()) {
-//            productList = dao.searchProductsByName(searchQuery);
-//        }
-Product latestProduct = dao.getLatestProduct();
-
         int productID = Integer.parseInt(request.getParameter("id"));
         Product product = dao.getProductById(productID);
         Vector<Brand> brandList = daoBrand.getAllBrands();
-        if (product == null) {
-            response.sendError(HttpServletResponse.SC_NOT_FOUND, "Product not found");
+        if(product == null){
+            response.sendError(HttpServletResponse.SC_NOT_FOUND,"Product not found");
             return;
         }
-        request.setAttribute("product", product);
+        request.setAttribute("product",product);
         request.setAttribute("brands", brandList);
-        request.setAttribute("latestProduct", latestProduct);
-//        request.setAttribute("productList", productList);
         request.getRequestDispatcher("WEB-INF/views/product-details.jsp").forward(request, response);
-    }
+    } 
 
-    /**
+    /** 
      * Handles the HTTP <code>POST</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -95,13 +83,12 @@ Product latestProduct = dao.getLatestProduct();
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
+    /** 
      * Returns a short description of the servlet.
-     *
      * @return a String containing servlet description
      */
     @Override
