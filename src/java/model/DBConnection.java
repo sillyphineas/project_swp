704d47baf31e6b5dd6e1320d08db04878a161564
LinhND-he172819 <linhndhe172819@fpt.edu.vn>
2025-03-07 23:55:06 +0700

    fe

--- src/java/model/DBConnection.java
+++ src/java/model/DBConnection.java
@@ -30,7 +30,7 @@ public class DBConnection {
         // Change URL, username, and password to match your MySQL server
 
 
-        this("jdbc:mysql://localhost:3306/proj_swp391_update1", "root", "");
+        this("jdbc:mysql://localhost:3306/proj_swp391_update1", "root", "123456789");
 
 
     }
